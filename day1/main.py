a67ec21393e1ae04db0234d4e51b6785474e6493
Vlad Vlasenko <vladvlasenko@Vlads-MacBook-Air.local>
2023-06-19 19:15:45 +0300

    add day1

---
+++ day1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator 2000!")
+city = input("Enter the city where you grew up:\n")
+pet_name = input("Enter name of your pet:\n")
+print("Your Band Name is: " + city + " " + pet_name)
