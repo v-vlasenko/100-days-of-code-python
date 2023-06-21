c34b4a6e90560eb4cbf1a72bcac0271b59e144a2
Vladyslav Vlasenko <fl4shqq@gmail.com>
2023-06-21 23:40:26 +0300

    day2 - add formating

--- day2/main.py
+++ day2/main.py
@@ -4,5 +4,6 @@ total_bill = float(input("What was the total bill? $"))
 percentage = int(input("What percentage bill would you like to give? 10, 12, or 15? "))
 number_of_people = int(input("How many people to split the bill? "))
 bill_for_each_man = round( (total_bill / number_of_people) * (percentage / 100 + 1), 2)
+bill_for_each_man = "{:.2f}".format(bill_for_each_man)
 
 print(f"Each person should pay: ${bill_for_each_man}")
