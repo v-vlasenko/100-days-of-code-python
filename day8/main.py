4de33b13566769bbf428ec0e79147bd2a29e5e5f
Vladyslav Vlasenko <vladyslav1595@gmail.com>
2023-07-09 16:22:22 +0300

    add user validation for 'cypher direction' input

--- day8/main.py
+++ day8/main.py
@@ -4,7 +4,7 @@ print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
-  output_text = ""
+  output_text = ""    
   #if user chooses 'decode' we must shift backwards
   if cipher_direction == "decode":
     shift_amount *= -1
@@ -22,18 +22,23 @@ continue_cypher = True
 
 while continue_cypher:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
-  
-  #if the user enters a shift that is greater than the number of letters in the alphabet:
-  shift %= 26
-  
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  
-  continue_or_no = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
   #validate user input
-  if continue_or_no != 'yes':
-    continue_cypher = False
-    print("Goodbye.")
+  if direction == 'encode' or direction == 'decode':
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    #if the user enters a shift that is greater than the number of letters in the alphabet:
+    shift %= 26
+    
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    
+    continue_or_no = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+    #validate user input
+    if continue_or_no != 'yes':
+        continue_cypher = False
+        print("Goodbye.")
+    else:
+        continue_cypher = True
   else:
-    continue_cypher = True
+    continue_cypher = False
+    print("Googbye!")
